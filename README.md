6d84f20db6b92501f244b23169254aab195b9316
jydhasan <s1910933181@ru.ac.bd>
2023-12-11 16:26:10 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<!-- generate readme.md file  -->
+# This app is create to make the 
+# pip install Flask Flask-SQLAlchemy pymysql
+# Your Project Name
+
+Brief description of your project.
+
+## Installation
+
+1. **Clone the Repository:**
+   ```bash
+   git clone https://github.com/your-username/your-repository.git
+   cd your-repository
+
---
+++ app.py
@@ -0,0 +1,62 @@
+# from flask import Flask, render_template, request
+# from flask_sqlalchemy import SQLAlchemy
+# import pandas as pd
+
+# app = Flask(__name__)
+# # Use the default SQLite database
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# db = SQLAlchemy(app)
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+
+app = Flask(__name__)
+# Update the MySQL connection string based on your XAMPP MySQL configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://jydhasan:124055@localhost/jydhasan'
+db = SQLAlchemy(app)
+
+
+
+
+class UploadedFile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(256), nullable=False)
+    table_name = db.Column(db.String(256), nullable=False)
+
+    def __repr__(self):
+        return f"<UploadedFile {self.filename}>"
+
+
+@app.route('/')
+def home():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    filename = file.filename
+
+    df = pd.read_excel(file)
+
+    # Generate a table name based on the filename (excluding extension)
+    table_name = 'table_' + filename[:-5]
+
+    # # Create the table in the database
+    # df.to_sql(table_name, db.engine, if_exists='replace')
+    # Create the table in the database
+    df.to_sql(table_name, db.engine, if_exists='replace', index=False)
+
+
+    uploaded_file = UploadedFile(filename=filename, table_name=table_name)
+    db.session.add(uploaded_file)
+    db.session.commit()
+
+    return f'File "{filename}" uploaded and table "{table_name}" created successfully!'
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
